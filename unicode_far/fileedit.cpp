a7f12fb7512598e1ef5e9f4a508bc4a40a791b0b
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-27 03:31:39 +0400

    Far3bis. Editor. Restore strOldCurDir after save

--- unicode_far/fileedit.cpp
+++ unicode_far/fileedit.cpp
@@ -1194,6 +1194,10 @@ int FileEditor::ReProcessKey(int Key,int CalledFromControl)
 					}
 				}
 
+				#if 1
+				//Maximus: возможно, этот фикс уже не нужен
+				FarChDir(strOldCurDir); // возможно правильнее выкинуть: FarChDir(strStartDir); - 2 вызова //???
+				#endif
 				return TRUE;
 			}
 			// $ 30.05.2003 SVS - Shift-F4 в редакторе/вьювере позволяет открывать другой редактор/вьювер (пока только редактор)
