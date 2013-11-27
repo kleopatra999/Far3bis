107ad52c88348ea40b27b9203700032e8b9baf7c
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-28 01:57:57 +0400

    Far3bis. Detect Far3wrapper, '2' in menus, FPF_FAR2 in API

--- unicode_far/plclass.hpp
+++ unicode_far/plclass.hpp
@@ -73,6 +73,11 @@ enum EXPORTS_ENUM
 	iOpenFilePlugin,
 	iGetMinFarVersion,
 
+	#if 1
+	//Maximus: поддержка Far3wrap
+	iWrapperFunction2, // FarWrapGetProcAddress
+	#endif
+
 	ExportsCount
 };
 
@@ -184,6 +189,10 @@ public:
 #ifndef NO_WRAPPER
 	virtual bool IsOemPlugin() const { return false; }
 #endif // NO_WRAPPER
+	#if 1
+	//Maximus: поддержка Far3wrap
+	virtual bool IsFar2Plugin() const { return HasWrapperFunction2(); }
+	#endif
 	virtual const string& GetHotkeyName() const { return m_strGuid; }
 
 	virtual bool InitLang(const string& Path);
@@ -227,6 +236,10 @@ public:
 
 	HAS_FUNCTION(OpenFilePlugin)
 	HAS_FUNCTION(GetMinFarVersion)
+	#if 1
+	//Maximus: поддержка Far3wrap
+	HAS_FUNCTION(WrapperFunction2)
+	#endif
 
 	#undef HAS_FUNCTION
 
