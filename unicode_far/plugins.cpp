7f8ac2f70bd45599d05ee96ecf647d5289b4480d
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-28 20:43:17 +0400

    Far3bis. Restrict ProcessConsoleInput in IsProcessAssignMacroKey

--- unicode_far/plugins.cpp
+++ unicode_far/plugins.cpp
@@ -1047,6 +1047,10 @@ int PluginManager::ProcessDialogEvent(int Event, FarDialogEvent *Param) const
 
 int PluginManager::ProcessConsoleInput(ProcessConsoleInputInfo *Info) const
 {
+	//Maximus: Ќаверное нефиг плагинам ковыр€тьс€, когда клавиша на макрос назначаетс€
+	if (IsProcessAssignMacroKey)
+		return 0;
+
 	int nResult = 0;
 
 	FOR(const auto& i, SortedPlugins)
